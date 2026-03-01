b28b03014ff01c7b22f87bd6e5c4dc220d0b746b
Florian Rumpel <florianrumpel@yahoo.com>
2026-03-01 10:18:05 +0100

    change landing page
    change font and layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 mt-4 justify-center items-center w-full ">
-      <p>Willkommen zu Valis Freundebuch!</p>
-
+    <div className="flex flex-col gap-4 mt-6 justify-center items-center w-full ">
+      <div className="text-center">
+        <p className="text-5xl font-bold font-pacifico text-pink-600">Valis</p>
+        <p className="font-bold text-3xl">Freundebuch</p>
+      </div>
+      <p className="w-1/2 md:max-w-1/3 ">
+        Willkommen in meinem Digitalen Freundebuch! Lerne meine Freunde kennen
+        oder hinterlasse selbst einen Eintrag fuer die Ewigkeit.
+      </p>
       <Link href={"/form"}>
         <Button>Sende eine Freundesanfrage</Button>
       </Link>
